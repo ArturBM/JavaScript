60081e1289dd65705351d3eb27e85498d3a57c61
ArturBM <arturbm05@gmail.com>
2024-02-22 14:29:00 -0300

    ta

---
+++ controle/for1.js
@@ -0,0 +1,20 @@
+let contador = 1
+while(contador <= 10) {
+    console.log(`contador = ${contador}`)
+    contador ++
+}
+
+for(let i = 1; i <= 10; i++) {
+    console.log(`i = ${i}`)
+}
+for(let i = 1; i <= 10; i++) {  //da no mesmo
+    console.log('i =', i)
+}
+
+const notas = [6.7, 7.4, 9.8, 8.1, 7.7]
+
+for(let i = 0; i < notas.length; i++)
+console.log(`nota = ${notas[i]}`)
+
+for(let i = 0; i < notas.length; i++) //da no mesmo 
+console.log('nota =', notas[i])
