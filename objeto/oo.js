1ad0a91d002800a653144a831c189e17b2c8a6ba
Artur Machado <arturbm05@gmail.com>
2024-08-04 15:42:14 -0300

    Curso Web Moderno

---
+++ objeto/oo.js
@@ -0,0 +1,22 @@
+// Código não executavel !!!!
+
+// Procedural
+processamento(valor1, valo2, valor3)
+
+// OO
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        //...
+    }
+}
+
+objeto.processamento() //foco passou a ser o objeto
+
+// principios importantes:
+// 1. Abstração
+// 2. Encapsulamento
+// 3. Herança (prototype)
+// 4. polimorfismo
